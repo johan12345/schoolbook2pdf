f426c967ee182b9114ee5d6d0cedd43251a81f25
Johan von Forstner <johan.forstner@gmail.com>
2016-07-06 14:38:57 +0200

    Add license

--- decrypter.py
+++ decrypter.py
@@ -1,4 +1,10 @@
 # coding=UTF-8
+
+# Copyright (c) 2016 Johan von Forstner
+#
+# This software may be modified and distributed under the terms
+# of the MIT license. See the LICENSE file for details.
+
 import os
 import sys
 import xml.etree.ElementTree as ET
